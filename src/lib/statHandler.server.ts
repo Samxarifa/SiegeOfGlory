43d3f838b40224d6948c8699a3099c681e75645b
Samxarifa <samuelhay9@gmail.com>
2024-02-12 14:29:40 +0000

    Changed getAuth to not spam api with new token requests when has valid token

--- src/lib/statHandler.server.ts
+++ src/lib/statHandler.server.ts
@@ -1,29 +1,38 @@
-import { R6AUTH } from '$env/static/private';
+import { R6CREDENTAILS } from '$env/static/private';
 
 const appId = '3587dcbb-7f81-457c-9781-0e3f29f6f56a';
 // const spaceId = '05bfb3f7-6c21-4c42-be1f-97a33fb5cf66';
 
+let ticket: string;
+let expire: string;
+// let sessionId: string;
+
+let nextTokenRefresh: number;
+
 async function getAuth() {
-	const out = await fetch('https://public-ubiservices.ubi.com/v3/profiles/sessions', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Ubi-AppId': appId,
-			Authorization: R6AUTH
-		},
-		body: JSON.stringify({
-			rememberMe: false
-		})
-	});
-	const obj = await out.json();
-	const sessionId: string = obj.sessionId;
-	const ticket: string = obj.ticket;
-	const expire: string = obj.expiration;
-	return { sessionId, ticket, expire };
+	if (!ticket || nextTokenRefresh <= new Date().getTime()) {
+		console.log('Requesting New Token');
+		const out = await fetch('https://public-ubiservices.ubi.com/v3/profiles/sessions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Ubi-AppId': appId,
+				Authorization: `Basic ${btoa(R6CREDENTAILS)}`
+			},
+			body: JSON.stringify({
+				rememberMe: false
+			})
+		});
+		const obj = await out.json();
+		// sessionId = obj.sessionId;
+		ticket = obj.ticket;
+		expire = obj.expiration;
+		nextTokenRefresh = new Date(expire).getTime();
+	}
 }
 
 export async function getPlayerIdByUsername(username: string, platform: string) {
-	const { ticket } = await getAuth();
+	await getAuth();
 
 	const out = await fetch(
 		`https://public-ubiservices.ubi.com/v2/profiles?platformType=${platform}&nameOnPlatform=${username}`,
