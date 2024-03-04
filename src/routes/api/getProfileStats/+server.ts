1f30eca0f85b4cad72b90f7369a45bf1c3a921ee
Samxarifa <samuelhay9@gmail.com>
2024-03-04 21:39:31 +0000

    Added 10 Minute Cache Header to R6API Fetch for Profile

--- src/routes/api/getProfileStats/+server.ts
+++ src/routes/api/getProfileStats/+server.ts
@@ -8,9 +8,12 @@ export async function GET(request: RequestEvent) {
 		const r6Data = await getProfilePageStats(r6Id);
 
 		if (r6Data) {
+			request.setHeaders({ 'Cache-Control': 'max-age=600' });
 			return json(r6Data);
+		} else {
+			return error(404, 'R6 ID not found');
 		}
 	} else {
-		return error(403, 'Forbidden');
+		return error(400, 'Missing R6 ID');
 	}
 }
