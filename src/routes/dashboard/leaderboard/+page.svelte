3bb9d8cd5821b553d49be20ad990f9ba8554d89a
Samxarifa <samuelhay9@gmail.com>
2024-03-27 13:07:20 +0000

    Fixed Bug: Animation Wasn't Firing

--- src/routes/dashboard/leaderboard/+page.svelte
+++ src/routes/dashboard/leaderboard/+page.svelte
@@ -1,36 +1,38 @@
 <script lang="ts">
 	import PodiumCard from '$lib/components/PodiumCard.svelte';
 	import LI from '$lib/components/LeaderboardItem.svelte';
-	import { fade } from 'svelte/transition';
+	import { tick } from 'svelte';
 
 	export let data;
 </script>
 
 <h1>Leaderboard</h1>
 {#if data.lb && data.lb.length > 2}
-	<section class="podium">
-		<PodiumCard
-			pos={2}
-			username={data.lb[1].username}
-			wins={data.lb[1].wins}
-			losses={data.lb[1].losses}
-		/>
-		<PodiumCard
-			pos={1}
-			username={data.lb[0].username}
-			wins={data.lb[0].wins}
-			losses={data.lb[0].losses}
-		/>
-		<PodiumCard
-			pos={3}
-			username={data.lb[2].username}
-			wins={data.lb[2].wins}
-			losses={data.lb[2].losses}
-		/>
-	</section>
+	{#await tick() then}
+		<section class="podium">
+			<PodiumCard
+				pos={2}
+				username={data.lb[1].username}
+				wins={data.lb[1].wins}
+				losses={data.lb[1].losses}
+			/>
+			<PodiumCard
+				pos={1}
+				username={data.lb[0].username}
+				wins={data.lb[0].wins}
+				losses={data.lb[0].losses}
+			/>
+			<PodiumCard
+				pos={3}
+				username={data.lb[2].username}
+				wins={data.lb[2].wins}
+				losses={data.lb[2].losses}
+			/>
+		</section>
+	{/await}
 {/if}
 {#if data.lb && data.lb.length > 1}
-	<section class="list" in:fade={{ delay: 600 }}>
+	<section class="list">
 		{#each data.lb as row}
 			{#if row.row > 3 || data.lb.length < 3}
 				<LI pos={row.row} username={row.username} wins={row.wins} losses={row.losses} />
